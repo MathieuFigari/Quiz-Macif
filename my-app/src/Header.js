395be67c330b9db38de9a2b34ee122c7215e3d8d
MathieuFigari <mathieu.figari@gmail.com>
2022-05-29 20:57:41 +0200

    title change ok

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return(
         <div className="header flex gap-20 items-center  w-screen h-[115px] bg-white drop-shadow-lg">
             <img  src="https://d1p2aimeyx6kdr.cloudfront.net/wp-content/uploads/2021/12/21144030/visuel_macif.jpg" className=" logo-macif max-w-full h-auto" alt="logo macif"/>
-            <h1 className="text-3xl font-bold">Macif</h1>
+            <h1 style={{width: "100%"}} className="text-3xl font-bold">QUIZ : Connaissez-vous la nouvelle offre Mobilité MACIF ?</h1>
             
            
         </div>
