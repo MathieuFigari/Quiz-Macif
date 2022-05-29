d464f53875606794ea831eaee24458c3c20aa9e0
MathieuFigari <mathieu.figari@gmail.com>
2022-05-29 20:49:45 +0200

    correction input

--- my-app/src/Answers.js
+++ my-app/src/Answers.js
@@ -7,7 +7,7 @@ const Answers = ({ option, submitAnswer }) => {
 
     return(
         <div className="form-check flex gap-4">
-        <input value={option} onChange={( e) => submitAnswer(e.target.value)}  className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id={option} />
+        <input  value={option} onChange={( e) => submitAnswer(e.target.value)}  className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id={option} style={{border: "1px solid black", width: "2rem" , height: "2rem"}}/>
         <label className=" form-check-label inline-block text-gray-800" htmlFor={option}>
           {option}
         </label>
